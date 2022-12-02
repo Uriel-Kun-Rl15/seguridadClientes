e14fef1e718e77b36025dbc1a24a51213b4ea15a
UTXJ Github <84731090+Uriel-200931@users.noreply.github.com>
2022-12-02 15:44:43 -0600

    sas
    asa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,3 +49,39 @@ import {
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+/*import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+//Agregados
+import { HttpClientModule } from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+*/
